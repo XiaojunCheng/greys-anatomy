e0bc70df0a6ec5de68b8813799054299fb3a703d
chengxiaojun <chengxiaojun@youzan.com>
2018-08-10 02:05:49 +0800

    新增debug模块

---
+++ debug/src/main/java/com/github/ompc/greys/debug/Bootstrap.java
@@ -0,0 +1,17 @@
+package com.github.ompc.greys.debug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Xiaojun.Cheng
+ * @date 2018/8/10
+ */
+@SpringBootApplication
+public class Bootstrap {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bootstrap.class, args);
+    }
+
+}
---
+++ debug/src/main/java/com/github/ompc/greys/debug/rest/UserController.java
@@ -0,0 +1,22 @@
+package com.github.ompc.greys.debug.rest;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+/**
+ * @author Xiaojun.Cheng
+ * @date 2018/8/10
+ */
+@RestController
+public class UserController {
+
+    @GetMapping(path = "/showTrace/{username}", produces = MediaType.TEXT_PLAIN_VALUE)
+    public String showTrace(@PathVariable String username) {
+        return username + "_" + new Date();
+    }
+
+}
